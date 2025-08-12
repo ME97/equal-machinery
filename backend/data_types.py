cb9098fab90cd43666ad5d202319bebbd77bc1a4
Miles Eastwood <eastwoodmiles@gmail.com>
2025-08-12 18:11:09 -0400

    changed to snake_case

--- backend/data_types.py
+++ backend/data_types.py
@@ -1,56 +1,49 @@
-from pydantic import BaseModel, field_validator, Field
+from pydantic import BaseModel, field_validator, Field, ConfigDict
 from typing import Optional
 from datetime import date
 from dataclasses import dataclass, field
 
-#TODO: Change fields to snake_case
-# like this:
-# def to_camel(string: str) -> str:
-#     parts = string.split('_')
-#     return parts[0] + ''.join(word.capitalize() for word in parts[1:])
+def to_camel(string: str) -> str:
+    parts = string.split('_')
+    return parts[0] + ''.join(word.capitalize() for word in parts[1:])
 
-# class MyBaseModel(BaseModel):
-#     model_config = ConfigDict(
-#         alias_generator=to_camel,
-#         populate_by_name=True
-#     )
-
-# class Driver(MyBaseModel):
-#     driver_id: int
-#     given_name: str
-#     family_name: str
+class MyBaseModel(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True
+    )
 
 @dataclass
 class DriverPair:
-    driverId1: int # driverId1 < driverId2
-    driverId2: int
-    constructorId: int
+    driver_id_1: int # driverId1 < driverId2
+    driver_id_2: int
+    ctor_id: int
     years: set[int] = field(default_factory=set) # years the drivers appear in at least one result together
     raceIds: set[int] = field(default_factory=set)
 
-class Driver(BaseModel):
-    driverId: int
-    driverRef: str
+class Driver(MyBaseModel):
+    driver_id: int
+    driver_ref: str
     number: Optional[int]
     code: str
     forename: str
     surname: str
     dob: date
     nationality: str
-    yearsActive: set[int] = Field(default_factory=set) # years that driver appears in at least one result
+    years_active: set[int] = Field(default_factory=set) # years that driver appears in at least one result
     teammates: set[int] = Field(default_factory=set) # list of teammates by driverId
-    driverPairs: set[tuple[int, int, int]] = Field(default_factory=set)
+    driver_pairs: set[tuple[int, int, int]] = Field(default_factory=set)
     def __str__(self):
         return f"{self.forename} {self.surname}"
     # maybe make teamate list (driverId, constructorId)
     # teams (which teams they were on, and for which time periods)
 
 # TODO: Add more fields to this (grid pos, finish pos, points, etc)
-class Result(BaseModel):
-    resultId: int
-    raceId: int
-    driverId: int
-    constructorId: int
+class Result(MyBaseModel):
+    result_id: int
+    race_id: int
+    driver_id: int
+    constructor_id: int
     position: Optional[int] # None if retired, DNF, etc (TODO: add field indicating this)
     
     @field_validator("position", mode="before")
@@ -59,16 +52,16 @@ class Result(BaseModel):
             return None
         return v
 
-    def result_str(self, driver_by_Id, race_by_Id):
+    def result_str(self, driver_by_id, race_by_id):
         if self.position:
-            return f"{driver_by_Id[self.driverId]} finished in P{self.position} in the {race_by_Id[self.raceId]}"
+            return f"{driver_by_id[self.driver_id]} finished in P{self.position} in the {race_by_id[self.race_id]}"
         else:
-            return f"{driver_by_Id[self.driverId]} did not finish the {race_by_Id[self.raceId]}"
+            return f"{driver_by_id[self.driver_id]} did not finish the {race_by_id[self.race_id]}"
 
-class Race(BaseModel):
-    raceId: int
+class Race(MyBaseModel):
+    race_id: int
     year: int
-    circuitId: int
+    circuit_id: int
     name: str
     date: date
     drivers: set[int] = Field(default_factory=set) # set of drivers who competed by driverId
@@ -77,12 +70,12 @@ class Race(BaseModel):
     def __str__(self):
         return f"{self.year} {self.name}"
 
-class Ctor(BaseModel):
-    constructorId: int
-    constructorRef: str
+class Ctor(MyBaseModel):
+    constructor_id: int
+    constructor_ref: str
     name: str
     nationality: str
-    driverPairIds: set[tuple[int, int, int]] = Field(default_factory=set)
+    driver_pair_ids: set[tuple[int, int, int]] = Field(default_factory=set)
 
 
 
--- backend/test.py
+++ backend/test.py
@@ -2,11 +2,11 @@ import csv
 from datetime import date
 from data_types import Driver, Race, Ctor, Result, DriverPair
 
-driver_by_Id: dict[int, Driver] = dict()
-race_by_Id: dict[int, Race] = dict()
-ctor_by_Id: dict[int, Ctor] = dict()
-result_by_Id: dict[int, Result] = dict()
-# returns dictionary with key as driverId, val as Driver object
+driver_by_id: dict[int, Driver] = dict()
+race_by_id: dict[int, Race] = dict()
+ctor_by_id: dict[int, Ctor] = dict()
+result_by_id: dict[int, Result] = dict()
+# returns dictionary with key as driver_id, val as Driver object
 
 
 def load_drivers(file_path: str) -> dict[int, Driver]:
@@ -50,80 +50,75 @@ def load_ctors(file_path: str) -> dict[int, Ctor]:
     return map
 
 
-def process_results(race_by_Id: dict[int, Race],
-                    result_by_Id: dict[int, Result],
-                    driver_by_Id: dict[int, Driver]):
-    for result in result_by_Id.values():
-        race: Race = race_by_Id[result.raceId]
-        race.results.add(result.resultId)
-        race.drivers.add(result.driverId)
-        race.ctors.add(result.constructorId)
-        driver_by_Id[result.driverId].yearsActive.add(race.date.year)
+def process_results(race_by_id: dict[int, Race],
+                    result_by_id: dict[int, Result],
+                    driver_by_id: dict[int, Driver]):
+    for result in result_by_id.values():
+        race: Race = race_by_id[result.race_id]
+        race.results.add(result.result_id)
+        race.drivers.add(result.driver_id)
+        race.ctors.add(result.constructor_id)
+        driver_by_id[result.driver_id].years_active.add(race.date.year)
 
 
-def populate_driver_pairings(race_by_Id: dict[int, Race],
-                             result_by_Id: dict[int, Result],
-                             driver_by_Id: dict[int, Driver],
-                             ctor_by_Id: dict[int, Ctor],
+def populate_driver_pairings(race_by_id: dict[int, Race],
+                             result_by_id: dict[int, Result],
+                             driver_by_id: dict[int, Driver],
+                             ctor_by_id: dict[int, Ctor],
                              ) -> dict[tuple[int, int, int], DriverPair]:
     driver_pair_by_id: dict[tuple[int, int, int], DriverPair] = dict()
 
-    for race in race_by_Id.values():
+    for race in race_by_id.values():
         for ctorId in race.ctors:
             pair: list[int, int] = list()
-            for resultId in race.results:
-                result: Result = result_by_Id[resultId]
-                if result.constructorId == ctorId:
-                    pair.append(result.driverId)
+            for result_id in race.results:
+                result: Result = result_by_id[result_id]
+                if result.constructor_id == ctorId:
+                    pair.append(result.driver_id)
 
             # temporary fix for if only one driver
             if len(pair) != 2:
                 continue
 
-            # smaller driverId goes first, to avoid duplicates
-            driverId1 = min(pair)
-            driverId2 = max(pair)
-            driverPairId = driverId1, driverId2, ctorId
+            # smaller driver_id goes first, to avoid duplicates
+            driver_id1 = min(pair)
+            driver_id2 = max(pair)
+            driver_pair_id = driver_id1, driver_id2, ctorId
 
-            if driverPairId not in driver_pair_by_id:
-                driver_pair_by_id[driverPairId] = DriverPair(
-                    driverId1, driverId2, ctorId)
+            if driver_pair_id not in driver_pair_by_id:
+                driver_pair_by_id[driver_pair_id] = DriverPair(
+                    driver_id1, driver_id2, ctorId)
 
-            driverPair: DriverPair = driver_pair_by_id[driverPairId]
-            driver_by_Id[driverId1].driverPairs.add(driverPairId)
-            driver_by_Id[driverId2].driverPairs.add(driverPairId)
-            ctor_by_Id[ctorId].driverPairIds.add(driverPairId)
+            driver_pair: DriverPair = driver_pair_by_id[driver_pair_id]
+            driver_by_id[driver_id1].driver_pairs.add(driver_pair_id)
+            driver_by_id[driver_id2].driver_pairs.add(driver_pair_id)
+            ctor_by_id[ctorId].driver_pair_ids.add(driver_pair_id)
 
-            driverPair.raceIds.add(race.raceId)
-            driverPair.years.add(race.date.year)
+            driver_pair.raceIds.add(race.race_id)
+            driver_pair.years.add(race.date.year)
 
     return driver_pair_by_id
 
 
 if __name__ == '__main__':
-    driver_by_Id = load_drivers('data/drivers.csv')
-    ctor_by_Id = load_ctors('data/constructors.csv')
-    result_by_Id = load_results('data/results.csv')
-    race_by_Id = load_races('data/races.csv')
-    process_results(race_by_Id, result_by_Id, driver_by_Id)
+    driver_by_id = load_drivers('data/drivers.csv')
+    ctor_by_id = load_ctors('data/constructors.csv')
+    result_by_id = load_results('data/results.csv')
+    race_by_id = load_races('data/races.csv')
+    process_results(race_by_id, result_by_id, driver_by_id)
 
     driver_pair_by_id: dict[tuple[int, int, int], DriverPair] = populate_driver_pairings(
-        race_by_Id, result_by_Id, driver_by_Id, ctor_by_Id)
+        race_by_id, result_by_id, driver_by_id, ctor_by_id)
 
-# for pairId in driver_by_Id[1].driverPairs:
-#     pair: DriverPair = driver_pair_by_id[pairId]
-#     print(
-#         f"{driver_by_Id[pair.driverId1]} was teammates with {driver_by_Id[pair.driverId2]}\
-#  at {ctor_by_Id[pair.constructorId].name} from {pair.firstRaceDate} to {pair.lastRaceDate}")
-mclarenDrivers = [driver_pair_by_id[tup]
-                  for tup in ctor_by_Id[1].driverPairIds]
-mclarenDrivers.sort(key=lambda p: min(p.years))
-print(mclarenDrivers[0])
+mclaren_drivers = [driver_pair_by_id[tup]
+                  for tup in ctor_by_id[1].driver_pair_ids]
+mclaren_drivers.sort(key=lambda p: min(p.years))
+print(mclaren_drivers[0])
 
-for pair in mclarenDrivers:
+for pair in mclaren_drivers:
     print(
-        f"{driver_by_Id[pair.driverId1]} was teammates with {driver_by_Id[pair.driverId2]}\
- at {ctor_by_Id[pair.constructorId].name} during {sorted(list(pair.years))}")
+        f"{driver_by_id[pair.driver_id_1]} was teammates with {driver_by_id[pair.driver_id_2]}\
+ at {ctor_by_id[pair.ctor_id].name} during {sorted(list(pair.years))}")
 
 
 # TODO: Update graphing functions to create edges based on pairings
